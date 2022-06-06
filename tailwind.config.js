8707b9d0248caa01d8b6c1625464d26e388d7bab
eXiga <anton.kostenich@hotmail.com>
2022-06-06 20:59:54 +0200

    Add custom daisy theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,ts,jsx,tsx}"],
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#ab97f4",
+          secondary: "#dda782",
+          accent: "#6838d1",
+          neutral: "#251D25",
+          "base-100": "#28385C",
+          info: "#8797D9",
+          success: "#41E1CF",
+          warning: "#B59008",
+          error: "#ED6E96",
+        },
+      },
+    ],
+  },
 };
