2f1a4658524439ef9a63a0aeb9024083d7ab90f3
eXiga <anton.kostenich@hotmail.com>
2022-09-12 16:54:11 +0200

    rework footer

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+interface BlogMeta {
+  github: string;
+  profilePicture: string;
+}
+
+const blogMeta: BlogMeta = {
+  github: "https://github.com/eXiga",
+  profilePicture: "https://github.com/eXiga.png",
+};
+
+export default blogMeta;
