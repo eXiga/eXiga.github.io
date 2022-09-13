118741ede6ae7bea9c92912bfaaf4d8483c53dcb
eXiga <anton.kostenich@hotmail.com>
2022-09-13 13:01:03 +0200

    add basic styling, add logos, add config file

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,13 @@
 interface BlogMeta {
   github: string;
   profilePicture: string;
+  logo: string;
 }
 
 const blogMeta: BlogMeta = {
   github: "https://github.com/eXiga",
   profilePicture: "https://github.com/eXiga.png",
+  logo: "../logo.svg",
 };
 
 export default blogMeta;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,ts,jsx,tsx}"],
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        cupcake: {
+          ...require("daisyui/src/colors/themes")["[data-theme=cupcake]"],
+          secondary: "#EB5991",
+        },
+      },
+    ],
+  },
 };
