b82659769be413427930ba185a9899a320ea5936
eXiga <anton.kostenich@hotmail.com>
2022-09-27 20:12:58 +0200

    change favicon, move logo to images, change paths

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,24 @@
+interface BlogTitles {
+  posts: string;
+  notFound: string;
+  about: string;
+}
 interface BlogMeta {
   github: string;
   profilePicture: string;
   logo: string;
+  titles: BlogTitles;
 }
 
 const blogMeta: BlogMeta = {
   github: "https://github.com/eXiga",
   profilePicture: "https://github.com/eXiga.png",
-  logo: "../logo.svg",
+  logo: "/images/logo.svg",
+  titles: {
+    posts: "Posts",
+    notFound: "Oopsie :(",
+    about: "About me",
+  },
 };
 
 export default blogMeta;
